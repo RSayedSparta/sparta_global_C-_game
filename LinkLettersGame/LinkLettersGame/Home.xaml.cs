6ff2d1ba57f2b8b3674f1896af5f35f21d538a5b
rahib <rsayed@spartaglobal.com>
2019-02-26 11:36:07 +0000

    All windows have been created for my application

---
+++ LinkLettersGame/LinkLettersGame/Home.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LinkLettersGame
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Medium med = new Medium();
+            med.Show();
+            this.Close();
+        }
+
+        private void EasyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+
+        private void HardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Hard hr = new Hard();
+            hr.Show();
+            this.Close();
+        }
+    }
+}
---
+++ LinkLettersGame/LinkLettersGame/SignUp.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LinkLettersGame
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPage_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn win = new LogIn();
+            win.Show();
+            this.Close();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn li = new LogIn();
+            this.Close();
+        }
+
+        public string checkPassword()
+        {
+            string password = "";
+
+            return password;
+        }
+
+        public string checkUsername()
+        {
+            string username = "";
+
+            return username;
+        }
+
+
+    }
+}
